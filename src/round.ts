fc069e5d92ff5f1b0d4c242f4abafb456d20dde3
Benedikt Franke <benedikt@franke.tech>
2021-09-02 16:01:56 +0200

    refactor: improve round description and define as function

--- src/round.ts
+++ src/round.ts
@@ -1,8 +1,9 @@
 /**
- * A round function using Math.round().js
+ * Round a number to a given number of decimal places.
+ *
  * https://gist.github.com/djD-REK/2e347f5532bb22310daf450f03ec6ad8
  */
-export const round = (number: number, decimalPlaces: number) => {
+export function round(number: number, decimalPlaces: number): number {
   const factorOfTen = 10 ** decimalPlaces;
   return Math.round(number * factorOfTen) / factorOfTen;
-};
+}
