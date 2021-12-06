181ef0abcc0538e9ad9d1c37c9247ef218bcef1a
spawnia <spawnia@users.noreply.github.com>
2021-12-06 14:33:01 +0000

    refactor: Apply eslint fix

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,14 +1,18 @@
-import {firstLine, includesIgnoreCase} from './string';
+import { firstLine, includesIgnoreCase } from './string';
 
 describe('includesIgnoreCase', () => {
   it('finds exact match', () => {
     expect(includesIgnoreCase('find me', 'search and find me')).toBeTruthy();
-    expect(includesIgnoreCase('find it', 'search and find me again')).toBeFalsy();
+    expect(
+      includesIgnoreCase('find it', 'search and find me again'),
+    ).toBeFalsy();
     expect(includesIgnoreCase('fist part', 'first part')).toBeFalsy();
   });
 
   it('finds no match', () => {
-    expect(includesIgnoreCase('find it', 'search and find me again')).toBeFalsy();
+    expect(
+      includesIgnoreCase('find it', 'search and find me again'),
+    ).toBeFalsy();
   });
 
   it('finds match', () => {
