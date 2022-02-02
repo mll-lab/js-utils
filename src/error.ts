fbc5f3356fa67f0a340fbc5bb570fe5faa36a8cc
Benedikt Franke <benedikt@franke.tech>
2022-02-02 16:33:38 +0100

    feat: add function errorMessage()
    Co-authored-by: mic-web <4804412+mic-web@users.noreply.github.com>

---
+++ src/error.ts
@@ -0,0 +1,19 @@
+/**
+ * Extract a message from a caught error.
+ *
+ * Since you can throw anything in JavaScript,
+ * the given error could be of any type.
+ */
+export function errorMessage(error: unknown): string {
+  const message = hasMessage(error) ? error.message : error;
+
+  if (message === undefined) {
+    return 'undefined';
+  }
+
+  return typeof message === 'string' ? message : JSON.stringify(error);
+}
+
+function hasMessage(error: unknown): error is { message: unknown } {
+  return typeof error === 'object' && error !== null && 'message' in error;
+}
