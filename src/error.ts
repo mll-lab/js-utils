8bedb665d05f7bf145f7971c2532a1dbf37922f7
Benedikt Franke <benedikt@franke.tech>
2022-02-02 16:26:21 +0100

    Update src/error.ts
    Co-authored-by: mic-web <4804412+mic-web@users.noreply.github.com>

--- src/error.ts
+++ src/error.ts
@@ -5,14 +5,15 @@
  * the given error could be of any type.
  */
 export function errorMessage(error: unknown): string {
-  const message =
-    typeof error === 'object' && error !== null && 'message' in error
-      ? (error as { message: unknown }).message
-      : error;
+  const message = hasMessage(error) ? error.message : error;
 
   if (message === undefined) {
     return 'undefined';
   }
 
   return typeof message === 'string' ? message : JSON.stringify(error);
+}
+
+function hasMessage(error: unknown): error is { message: unknown } {
+  return typeof error === "object" && error !== null && "message" in error;
 }
