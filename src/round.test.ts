de14cf522dde2ac1be70ba0f186964670d06ec4a
Simon Bigelmayr <s.bigelmayr@gmx.net>
2022-03-14 16:14:08 +0100

    feature: add function to return first decimal digit of a number (#7)

--- src/round.test.ts
+++ src/round.test.ts
@@ -1,4 +1,4 @@
-import { round } from './round';
+import { firstDecimalDigit, round } from './round';
 
 describe('round', () => {
   it.each([
@@ -12,4 +12,17 @@ describe('round', () => {
       expect(round(number, decimals)).toEqual(expectedResult);
     },
   );
+});
+
+describe('firstDecimalDigit', () => {
+  it.each([
+    [1, 0],
+    [0.2345, 2],
+    [11.5344, 5],
+    [-1.7345, 7],
+    [-7, 0],
+    [1.67, 6],
+  ])('returns the decimal digit of a number', (number, expectedResult) => {
+    expect(firstDecimalDigit(number)).toEqual(expectedResult);
+  });
 });
--- src/round.ts
+++ src/round.ts
@@ -7,3 +7,18 @@ export function round(number: number, decimalPlaces: number): number {
   const factorOfTen = 10 ** decimalPlaces;
   return Math.round(number * factorOfTen) / factorOfTen;
 }
+
+export function firstDecimalDigit(number: number): number {
+  if (number % 1 === 0) {
+    return 0;
+  }
+
+  const regExp = /\d*\.(\d)/;
+  const regExpMatchArray = number.toString().match(regExp);
+
+  if (regExpMatchArray === null) {
+    throw new Error(`Invalid number for regex matching: ${number}`);
+  }
+
+  return Number(regExpMatchArray[1]);
+}
