fd6d716cc579810a5843976fbd27646da169c164
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2022-06-29 14:10:47 +0200

    refactor: it is a suffix

--- src/style.ts
+++ src/style.ts
@@ -1,11 +1,11 @@
 /**
  * Converts pixel value to number (e.g. "4px" to 4)
  */
-const PIXEL_PREFIX = 'px';
+const PIXEL_SUFFIX = 'px';
 
 export function pxToNumber(pixels: string): number {
   const count = (pixels.match(/px/g) || []).length;
-  if (count > 1 || !pixels.endsWith(PIXEL_PREFIX)) {
+  if (count > 1 || !pixels.endsWith(PIXEL_SUFFIX)) {
     throw new Error(`'${pixels}' does not contain a single 'px'`);
   }
 
