d71a170a33cbbc254c37d7f25f022c3d8dd50c12
Benedikt Franke <benedikt.franke@mll.com>
2022-07-12 14:50:14 +0200

    feat: add array function toggleElement

--- src/array.test.ts
+++ src/array.test.ts
@@ -3,7 +3,7 @@ import {
   insertIf,
   isNonEmptyArray,
   last,
-  NonEmptyArray,
+  NonEmptyArray, toggleElement,
   withoutIndex,
 } from './array';
 
@@ -77,5 +77,19 @@ describe('last', () => {
 
   it('returns undefined for empty arrays', () => {
     expect(last([])).toEqual(undefined);
+  });
+});
+
+describe('toggleElement', () => {
+  it('removes existing element', () => {
+    expect(toggleElement([1], 1)).toEqual([]);
+  });
+
+  it('appends non-existing element', () => {
+    expect(toggleElement([], 1)).toEqual([1]);
+  });
+
+  it('uses strict comparison', () => {
+    expect(toggleElement([1, '2'], '1')).toEqual([1, '2', '1']);
   });
 });
