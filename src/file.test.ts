eace6215bb3677b56998640a3afb19f17eb1b4f4
Simon Bigelmayr <simon.bigelmayr@mll.com>
2022-09-12 16:02:31 +0200

    feature: add file download

---
+++ src/file.test.ts
@@ -0,0 +1,11 @@
+import {downloadBlob} from "./file";
+
+describe('download', () => {
+  const documentIntial = new Blob(['foo']);
+  global.URL.createObjectURL = jest.fn();
+  it('createObjectURL should be called with content once', () => {
+    downloadBlob(documentIntial, 'test.csv');
+    expect(global.URL.createObjectURL).toHaveBeenCalledTimes(1);
+    expect(global.URL.createObjectURL).toHaveBeenCalledWith(documentIntial)
+  });
+});
