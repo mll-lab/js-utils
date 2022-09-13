69834a474e0a2b52aac292c65ae8ba9262832b54
Simon Bigelmayr <s.bigelmayr@gmx.net>
2022-09-13 11:00:59 +0200

    feature: add file download (#14)

---
+++ src/file.ts
@@ -0,0 +1,9 @@
+export function downloadBlob(blob: Blob, filename: string): void {
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './array';
 export * from './date';
 export * from './device';
 export * from './error';
+export * from './file';
 export * from './germanNumber';
 export * from './number';
 export * from './pluralize';
