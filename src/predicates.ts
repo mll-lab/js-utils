50a7aa0da7e1784e4adaedf735d0b90d17357661
Benedikt Franke <benedikt.franke@mll.com>
2022-10-07 15:43:44 +0200

    refactor: avoid arrow functions

--- src/predicates.ts
+++ src/predicates.ts
@@ -4,29 +4,33 @@ export function isString(value: unknown): value is string {
   return typeof value === 'string';
 }
 
-export const isAlphanumeric: PredicateFn = (value) =>
-  isString(value) && /^[A-Za-z0-9]+$/.test(value);
+export const isAlphanumeric: PredicateFn = function (value): value is string {
+  return isString(value) && /^[A-Za-z0-9]+$/.test(value);
+};
 
 /**
  * BSNR (Betriebsstättennummer) ist eine eindeutige Zuordnung von Leistungen zu dem entsprechenden Ort der Leistungserbringung ermöglicht,
  * für alle vertrags(zahn)ärztlichen Leistungserbringer gültig
  * und klar abzugrenzen vom Institutskennzeichen (IK-Nummer) eines Krankenhauses.
  */
-export const isBSNR: PredicateFn = (value) =>
-  isString(value) && /^\d{9}$/.test(value);
+export const isBSNR: PredicateFn = function (value): value is string {
+  return isString(value) && /^\d{9}$/.test(value);
+};
 
 // Taken from https://emailregex.com
 export const EMAIL_REGEX =
   // eslint-disable-next-line no-useless-escape
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const isEmail: PredicateFn = (value) =>
-  isString(value) && EMAIL_REGEX.test(value);
+export const isEmail: PredicateFn = function (value): value is string {
+  return isString(value) && EMAIL_REGEX.test(value);
+};
 
-export const isOnlyDigits: PredicateFn = (value) =>
-  isString(value) && /^\d+$/.test(value);
+export const isOnlyDigits: PredicateFn = function (value): value is string {
+  return isString(value) && /^\d+$/.test(value);
+};
 
-export const isURL: PredicateFn = (value) => {
+export const isURL: PredicateFn = function (value): value is string {
   if (!isString(value)) {
     return false;
   }
@@ -41,14 +45,17 @@ export const isURL: PredicateFn = (value) => {
   }
 };
 
-export const isWord: PredicateFn = (value) =>
-  isString(value) && /^[\w-]+$/.test(value);
+export const isWord: PredicateFn = function (value): value is string {
+  return isString(value) && /^[\w-]+$/.test(value);
+};
 
-export const isLabId: PredicateFn = (value) =>
-  isString(value) && /^\d{2}-\d{6}$/.test(value);
+export const isLabId: PredicateFn = function (value): value is string {
+  return isString(value) && /^\d{2}-\d{6}$/.test(value);
+};
 
-export const isRackBarcode: PredicateFn = (value) =>
-  isString(value) && /^[A-Z]{2}\d{8}$/.test(value);
+export const isRackBarcode: PredicateFn = function (value): value is string {
+  return isString(value) && /^[A-Z]{2}\d{8}$/.test(value);
+};
 
 export function isNotNullish<T>(value: T | null | undefined): value is T {
   return value != null;
