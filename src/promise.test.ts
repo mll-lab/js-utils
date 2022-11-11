0141d34b85c5b687d751f4d79aa1cfe5cda8a993
Benedikt Franke <benedikt.franke@mll.com>
2022-11-11 11:43:05 +0100

    feat: add promise functions `callSequentially` and `mapSequentially`

---
+++ src/promise.test.ts
@@ -0,0 +1,46 @@
+import {callSequentially, mapSequentially } from './promise';
+
+describe('callSequentially', () => {
+  it('throws and aborts if callback throws', async () => {
+    const foo = 'foo';
+    const first = jest.fn().mockImplementation(() => {
+      throw new Error(foo);
+    });
+    const second = jest.fn();
+
+    await expect(async () => {
+      await callSequentially([first, second]);
+    }).rejects.toThrow(foo);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe('mapSequentially', () => {
+  it('returns empty array', async () => {
+    expect(await mapSequentially([], () => Promise.resolve())).toEqual([]);
+  });
+
+  it('requires promises', async () => {
+    // @ts-expect-error should fail to compile, since it is equivalent to just .map()
+    await mapSequentially([], () => 1);
+  });
+
+  it('maps values', async () => {
+    expect(
+      await mapSequentially([1, 2], (value) => Promise.resolve(value * 2)),
+    ).toEqual([2, 4]);
+  });
+
+  it('throws and aborts if callback throws', async () => {
+    const callback = jest.fn().mockImplementation((value) => {
+      throw new Error(value);
+    });
+
+    const foo = 'foo';
+    await expect(async () => {
+      await mapSequentially([foo, 'bar'], callback);
+    }).rejects.toThrow(foo);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
