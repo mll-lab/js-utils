cd9fc429984ec7c48b3b04327ff5cb072d371a5e
spawnia <spawnia@users.noreply.github.com>
2022-12-07 15:42:25 +0000

    refactor: Apply eslint fix

--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -2,8 +2,6 @@ export function isString(value: unknown): value is string {
   return typeof value === 'string';
 }
 
-export function isNotNullish<T>(
-  value: T,
-): value is NonNullable<T> {
+export function isNotNullish<T>(value: T): value is NonNullable<T> {
   return value != null;
 }
