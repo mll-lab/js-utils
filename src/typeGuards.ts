ca3baaefa501f6e7e1759102755f3fc58d6fe3e5
Benedikt Franke <benedikt.franke@mll.com>
2024-04-22 09:43:20 +0200

    feat: add isNonEmptyString

--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -2,15 +2,16 @@ export function isString(value: unknown): value is string {
   return typeof value === 'string';
 }
 
+export function isNonEmptyString(value: unknown): value is Exclude<string, ''> {
+  return isString(value) && value !== '';
+}
+
 export function isNotNullish<T>(value: T): value is NonNullable<T> {
   return value != null;
 }
 
 export function isArrayOfStrings(value: unknown): value is Array<string> {
-  return (
-    value instanceof Array &&
-    value.every((record) => typeof record === 'string')
-  );
+  return value instanceof Array && value.every(isString);
 }
 
 export function isEmptyObject(value: unknown): value is Record<string, never> {
